29979bb00a4c379fd9a4e529fb202931a3728a19
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-10-26 20:13:36 +1100

    change for deploy

--- Api/server.js
+++ Api/server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "client/public")));
 app.use(
   express.json({
     verify: (req, res, buf) => {
@@ -18,10 +17,11 @@ app.use(
   );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 }
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./Client/build/index.html"));
 });
 
 app.post("/", async (req, res) => {
@@ -57,7 +57,7 @@ app.post("/", async (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(PORT, () => {
