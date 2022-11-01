6b538b3a1813a09e3981dff549f1e426accef703
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-11-01 20:06:38 +1100

    add

--- Api/server.js
+++ Api/server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(
   express.json({
     verify: (req, res, buf) => {
@@ -16,10 +17,9 @@ app.use(
   })
   );
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Client/build", "index.html"));
+});
 
 app.post("/", async (req, res) => {
   const transporter = nodemailer.createTransport({
@@ -53,10 +53,6 @@ app.post("/", async (req, res) => {
   console.log(response);
 });
 
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/Client/build", "index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
